8328b034c3f93d7baf0bae128b128b528ad2a44e
Victor Ruggi <victorruggiitalian@hotmail.com>
2016-11-30 01:54:14 +0000

    Update AboutActivity.java

--- app/src/main/java/com/radiantridge/restoradiantridge/AboutActivity.java
+++ app/src/main/java/com/radiantridge/restoradiantridge/AboutActivity.java
@@ -1,8 +1,13 @@
 package com.radiantridge.restoradiantridge;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -24,19 +29,53 @@ public class AboutActivity extends Menu {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-
         LinearLayout ll = (LinearLayout)findViewById(R.id.LL);
         TextView tv1 = new TextView(this);
-        tv1.setText(R.string.menu_about);
-        tv1.setTextSize(28);
-        tv1.setTextColor(Color.RED);
+        tv1.setText(R.string.about_header);
+        tv1.setTextSize(40);
+        tv1.setTextColor(Color.parseColor("#03CA00")); //Some nice forest GREEN.
+        tv1.setClickable(true);
+        tv1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.dawsoncollege.qc.ca";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
         ll.addView(tv1);
 
         TextView tv2 = new TextView(this);
-        tv2.setText(R.string.about_menu_text);
-        tv1.setTextSize(20);
-        tv1.setTextColor(Color.parseColor("#03CA00")); /*Green*/
+        tv2.setText(R.string.course_desc);
+        tv2.setTextSize(24);
+        tv2.setTextColor(Color.parseColor("#00A080")); //How about some San Jose Sharks TEAL?
+        tv2.setClickable(true);
+        tv2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.dawsoncollege.qc.ca/computer-science-technology/";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
         ll.addView(tv2);
+
+        ImageView iv3 = new ImageView(this);
+        iv3.setImageResource(R.drawable.grouppic);
+        iv3.setClickable(true);
+        iv3.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+                builder.setMessage(R.string.alertbox_text).setTitle(R.string.alertbox_title).setCancelable(true);
+                AlertDialog ad = builder.create();
+                ad.show();
+            }
+        });
+        ll.addView(iv3);
 
     }
 }
