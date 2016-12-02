f637d94b304180022537a4e0cd5384bea85c63d3
Rafia Anwar <rafia.anwar95@gmail.com>
2016-12-01 20:40:27 -0500

    updated resto class. added float rating and int price range

--- app/src/main/java/com/radiantridge/restoradiantridge/Restaurant.java
+++ app/src/main/java/com/radiantridge/restoradiantridge/Restaurant.java
@@ -4,6 +4,7 @@ import java.sql.Timestamp;
 
 /**
  * Created by 1141669 on 11/21/2016.
+ * @author rafia
  */
 
 public class Restaurant {
@@ -14,16 +15,17 @@ public class Restaurant {
     private String addCity;
     private String addPostalCode;
     private String genre;
-    private String priceRange; //the range is $ to $$$$
+    private int priceRange; //the range is 1 to 4
     private Timestamp createdTime;  // ?? creation time of what
     private Timestamp modifiedTime;  // ?? of what
-    private int starRating; // 1- 5
+    private float starRating; // 1- 5
     private String notes;
     private double longitude;
     private double latitude;
+    private int dbId;
+    private String phone;
 
     public Restaurant() {
-
     }
 
     public String getName() {
@@ -74,11 +76,11 @@ public class Restaurant {
         this.genre = genre;
     }
 
-    public String getPriceRange() {
+    public int getPriceRange() {
         return priceRange;
     }
 
-    public void setPriceRange(String priceRange) {
+    public void setPriceRange(int priceRange) {
         this.priceRange = priceRange;
     }
 
@@ -98,11 +100,11 @@ public class Restaurant {
         this.modifiedTime = modifiedTime;
     }
 
-    public int getStarRating() {
+    public float getStarRating() {
         return starRating;
     }
 
-    public void setStarRating(int starRating) {
+    public void setStarRating(float starRating) {
         this.starRating = starRating;
     }
 
@@ -130,6 +132,22 @@ public class Restaurant {
         this.latitude = latitude;
     }
 
+    public void setDatabaseId(int id) {
+        this.dbId = id;
+    }
+
+    public int getDatabaseId() {
+        return this.dbId;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -138,22 +156,23 @@ public class Restaurant {
         Restaurant that = (Restaurant) o;
 
         if (addNum != that.addNum) return false;
-        if (starRating != that.starRating) return false;
+        if (priceRange != that.priceRange) return false;
+        if (Float.compare(that.starRating, starRating) != 0) return false;
         if (Double.compare(that.longitude, longitude) != 0) return false;
         if (Double.compare(that.latitude, latitude) != 0) return false;
+        if (dbId != that.dbId) return false;
         if (!name.equals(that.name)) return false;
         if (addStreet != null ? !addStreet.equals(that.addStreet) : that.addStreet != null)
             return false;
         if (addCity != null ? !addCity.equals(that.addCity) : that.addCity != null) return false;
-        if (!addPostalCode.equals(that.addPostalCode)) return false;
+        if (addPostalCode != null ? !addPostalCode.equals(that.addPostalCode) : that.addPostalCode != null)
+            return false;
         if (genre != null ? !genre.equals(that.genre) : that.genre != null) return false;
-        if (priceRange != null ? !priceRange.equals(that.priceRange) : that.priceRange != null)
-            return false;
-        if (createdTime != null ? !createdTime.equals(that.createdTime) : that.createdTime != null)
-            return false;
+        if (!createdTime.equals(that.createdTime)) return false;
         if (modifiedTime != null ? !modifiedTime.equals(that.modifiedTime) : that.modifiedTime != null)
             return false;
-        return notes != null ? notes.equals(that.notes) : that.notes == null;
+        if (notes != null ? !notes.equals(that.notes) : that.notes != null) return false;
+        return phone != null ? phone.equals(that.phone) : that.phone == null;
 
     }
 
@@ -165,17 +184,19 @@ public class Restaurant {
         result = 31 * result + addNum;
         result = 31 * result + (addStreet != null ? addStreet.hashCode() : 0);
         result = 31 * result + (addCity != null ? addCity.hashCode() : 0);
-        result = 31 * result + addPostalCode.hashCode();
+        result = 31 * result + (addPostalCode != null ? addPostalCode.hashCode() : 0);
         result = 31 * result + (genre != null ? genre.hashCode() : 0);
-        result = 31 * result + (priceRange != null ? priceRange.hashCode() : 0);
-        result = 31 * result + (createdTime != null ? createdTime.hashCode() : 0);
+        result = 31 * result + priceRange;
+        result = 31 * result + createdTime.hashCode();
         result = 31 * result + (modifiedTime != null ? modifiedTime.hashCode() : 0);
-        result = 31 * result + starRating;
+        result = 31 * result + (starRating != +0.0f ? Float.floatToIntBits(starRating) : 0);
         result = 31 * result + (notes != null ? notes.hashCode() : 0);
         temp = Double.doubleToLongBits(longitude);
         result = 31 * result + (int) (temp ^ (temp >>> 32));
         temp = Double.doubleToLongBits(latitude);
         result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + dbId;
+        result = 31 * result + (phone != null ? phone.hashCode() : 0);
         return result;
     }
 
@@ -188,13 +209,15 @@ public class Restaurant {
                 ", addCity='" + addCity + '\'' +
                 ", addPostalCode='" + addPostalCode + '\'' +
                 ", genre='" + genre + '\'' +
-                ", priceRange='" + priceRange + '\'' +
+                ", priceRange=" + priceRange +
                 ", createdTime=" + createdTime +
                 ", modifiedTime=" + modifiedTime +
                 ", starRating=" + starRating +
                 ", notes='" + notes + '\'' +
                 ", longitude=" + longitude +
                 ", latitude=" + latitude +
+                ", dbId=" + dbId +
+                ", phone='" + phone + '\'' +
                 '}';
     }
 }
