5bfc605d2295f669466b3e75eeb2453249bb3394
Rafia Anwar <rafia.anwar95@gmail.com>
2016-12-08 03:27:47 -0500

    updated showReviewAct

--- app/src/main/java/com/radiantridge/restoradiantridge/activities/ShowReviewActivity.java
+++ app/src/main/java/com/radiantridge/restoradiantridge/activities/ShowReviewActivity.java
@@ -1,10 +1,29 @@
 package com.radiantridge.restoradiantridge.activities;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.radiantridge.restoradiantridge.R;
+import com.radiantridge.restoradiantridge.objects.Review;
+
 /**
  * Created by Rafia on 2016-12-08.
  */
 
-public class ShowReviewActivity {
+public class ShowReviewActivity extends AppCompatActivity {
+    private static final String TAG = "Show review Act";
+    private Review review;
+    /**
+     * Overridden Lifecycle method.
+     * @param savedInstanceState stores the value when screen rotated
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_review);
 
+        review = new Review();
+
+    }
 }
 
