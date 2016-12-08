827eb9a31f64b1817ae54ae231a46b7c1cd5f575
Victor Ruggi <victorruggiitalian@hotmail.com>
2016-12-08 03:46:50 +0000

    Update AboutActivity.java

--- app/src/main/java/com/radiantridge/restoradiantridge/activities/AboutActivity.java
+++ app/src/main/java/com/radiantridge/restoradiantridge/activities/AboutActivity.java
@@ -6,13 +6,14 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
-
-import com.radiantridge.restoradiantridge.R;
 
 /**
 * This activity tells you what our application does.
@@ -21,7 +22,7 @@ import com.radiantridge.restoradiantridge.R;
 * @author Victor
 */
 
-public class AboutActivity extends MenuActivity {
+public class AboutActivity extends Menu {
 
 	/**
 	* Minimum override method
@@ -65,8 +66,12 @@ public class AboutActivity extends MenuActivity {
         ll.addView(tv2);
 
         final Context context = AboutActivity.this;
+
+        LinearLayout ll2 = new LinearLayout(this);
+
+        ll2.setOrientation(LinearLayout.HORIZONTAL);
         ImageView iv3 = new ImageView(this);
-        iv3.setImageResource(R.drawable.grouppic);
+        iv3.setImageResource(R.drawable.aline);
         iv3.setClickable(true);
         iv3.setOnClickListener(new View.OnClickListener() {
 
@@ -82,7 +87,7 @@ public class AboutActivity extends MenuActivity {
 
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss(); //Dismisses the dialog
+                        dialog.dismiss(); //Dismisses the dialogue
                     }
                 });
 
@@ -93,7 +98,6 @@ public class AboutActivity extends MenuActivity {
                     public void onClick(DialogInterface dialog, int which) {
                         Intent i = new Intent (getApplicationContext(), MainActivity.class);
                         startActivity(i);
-                        dialog.dismiss(); //Dismisses the dialog
                     }
                 });
 
@@ -101,7 +105,120 @@ public class AboutActivity extends MenuActivity {
                 ad.show();
             }
         });
-        ll.addView(iv3);
+        ll2.addView(iv3);
+
+        ImageView iv4 = new ImageView(this);
+        iv4.setImageResource(R.drawable.erika);
+        iv4.setClickable(true);
+        iv4.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context); //New alert dialog based on this current context
+                builder.setMessage(R.string.alertbox_text);
+                builder.setTitle(R.string.alertbox_title);
+                builder.setCancelable(true);
+
+                /*When clicked, this will simply close the dialog and display what was shown on the AboutActivity Screen.*/
+                builder.setPositiveButton(R.string.close, new DialogInterface.OnClickListener(){
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss(); //Dismisses the dialogue
+                    }
+                });
+
+                /*When clicked, this will take the user back to the main menu.*/
+                builder.setNegativeButton(R.string.to_main_menu, new DialogInterface.OnClickListener(){
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent i = new Intent (getApplicationContext(), MainActivity.class);
+                        startActivity(i);
+                    }
+                });
+
+                AlertDialog ad = builder.create();
+                ad.show();
+            }
+        });
+        ll2.addView(iv4);
+
+        ImageView iv5 = new ImageView(this);
+        iv5.setImageResource(R.drawable.rafia);
+        iv5.setClickable(true);
+        iv5.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context); //New alert dialog based on this current context
+                builder.setMessage(R.string.alertbox_text);
+                builder.setTitle(R.string.alertbox_title);
+                builder.setCancelable(true);
+
+                /*When clicked, this will simply close the dialog and display what was shown on the AboutActivity Screen.*/
+                builder.setPositiveButton(R.string.close, new DialogInterface.OnClickListener(){
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss(); //Dismisses the dialogue
+                    }
+                });
+
+                /*When clicked, this will take the user back to the main menu.*/
+                builder.setNegativeButton(R.string.to_main_menu, new DialogInterface.OnClickListener(){
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent i = new Intent (getApplicationContext(), MainActivity.class);
+                        startActivity(i);
+                    }
+                });
+
+                AlertDialog ad = builder.create();
+                ad.show();
+            }
+        });
+        ll2.addView(iv5);
+
+        ImageView iv6 = new ImageView(this);
+        iv6.setImageResource(R.drawable.aline);
+        iv6.setClickable(true);
+        iv6.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context); //New alert dialog based on this current context
+                builder.setMessage(R.string.alertbox_text);
+                builder.setTitle(R.string.alertbox_title);
+                builder.setCancelable(true);
+
+                /*When clicked, this will simply close the dialog and display what was shown on the AboutActivity Screen.*/
+                builder.setPositiveButton(R.string.close, new DialogInterface.OnClickListener(){
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss(); //Dismisses the dialogue
+                    }
+                });
+
+                /*When clicked, this will take the user back to the main menu.*/
+                builder.setNegativeButton(R.string.to_main_menu, new DialogInterface.OnClickListener(){
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent i = new Intent (getApplicationContext(), MainActivity.class);
+                        startActivity(i);
+                    }
+                });
+
+                AlertDialog ad = builder.create();
+                ad.show();
+            }
+        });
+        ll2.addView(iv6);
+
+        ll.addView(ll2);
 
     }
 }
