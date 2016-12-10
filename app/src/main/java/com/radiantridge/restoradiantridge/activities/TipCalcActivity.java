5b3144562b0590eab2ffe91018997ce338ec1717
Rafia Anwar <rafia.anwar95@gmail.com>
2016-12-09 20:08:38 -0500

    updated tipcalc act

--- app/src/main/java/com/radiantridge/restoradiantridge/activities/TipCalcActivity.java
+++ app/src/main/java/com/radiantridge/restoradiantridge/activities/TipCalcActivity.java
@@ -12,66 +12,77 @@ import android.widget.TextView;
 import com.radiantridge.restoradiantridge.R;
 
 /**
+ * This activity calculates the amount of tip when user inputs
+ * the bill amount.
  *
  * @author Rafia Anwar
+ * @version 30/11/2016
  */
-public class TipCalcActivity extends AppCompatActivity {
-    private static final String TAG = "Tip Act";
 
-    private String billStr;
-    private RadioGroup radioGroup;
-    private RadioButton radioButton;
+public class TipCalcActivity extends AppCompatActivity {
+    private static final String TAG = "TipCalculatorActivity";
+
+    /**
+     * Overridden lifecycle method.
+     *
+     * @param savedInstanceState savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tip_calc);
     }
-    public void onCalculateTip(View v) {
-        //switch between modes
 
+    /**
+     * This method takes the user input from the form, calculates and
+     * displays the amount of tip and total amount i.e. bill with tip included.
+     *
+     * @param v View that fired the event
+     */
+    public void calculateTip(View v) {
         EditText billAmount = (EditText) findViewById(R.id.editText);
-        //check for null and empty string
-//show error, make a new field, hide show it
+        String billStr = billAmount.getText().toString();   // represents the amount of bill
 
-        billStr = billAmount.getText().toString();
-        if(billStr==null || billStr.isEmpty()) {
-            //show error label
-            TextView amountErr= (TextView) findViewById(R.id.textAmountError);
+        // checks for null or empty field
+        if (billStr == null || billStr.isEmpty()) {
+            //displays error label if null or empty
+            TextView amountErr = (TextView) findViewById(R.id.textAmountError);
             amountErr.setVisibility(View.VISIBLE);
-
-        }
-        else {
+        } else {
             Double bill = Double.parseDouble(billStr);
-            if(bill <= 0)
-            {
-                TextView amountErr= (TextView) findViewById(R.id.textAmountError);
+
+            // displays error label when bill amount is not greater than 0
+            if (bill <= 0) {
+                TextView amountErr = (TextView) findViewById(R.id.textAmountError);
                 amountErr.setVisibility(View.VISIBLE);
-            }
-            else {
-                radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+            } else {
+                // var for radio group containing radio buttons for tip %
+                RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+
                 int selectedId = radioGroup.getCheckedRadioButtonId();
                 Log.i(TAG, "selected id " + selectedId);
-                // find the radiobutton by returned id
-                if(selectedId != -1) {
-                    radioButton = (RadioButton) findViewById(selectedId);
+
+                // find the radio button by returned id
+                if (selectedId != -1) {
+                    // selected radio button for tip %
+                    RadioButton radioButton = (RadioButton) findViewById(selectedId);
                     String tipRate = radioButton.getText().toString();
                     String tipStr = tipRate.substring(0, 2); //gets the numerical value
                     int tip = Integer.parseInt(tipStr);
 
                     Double calcTip = (bill / 100) * tip;
-                    Double calculatedTip = Math.round(calcTip * 100.0) / 100.0;
+                    Double roundedCalcTip = Math.round(calcTip * 100.0) / 100.0;
 
-                    Double billPlusTip = bill + calculatedTip;
-                    Double billAndTip = Math.round(billPlusTip * 100.0) / 100.0;
+                    Double billPlusTip = bill + roundedCalcTip;
+                    Double roundedBillPlusTip = Math.round(billPlusTip * 100.0) / 100.0;
 
                     TextView tipTextView = (TextView) findViewById(R.id.textView6);
-                    tipTextView.setText(calculatedTip.toString());
+                    tipTextView.setText(roundedCalcTip.toString());
 
                     TextView totalTextView = (TextView) findViewById(R.id.textView8);
-                    totalTextView.setText(billAndTip.toString());
+                    totalTextView.setText(roundedBillPlusTip.toString());
                 }
             }
         }
     }
-
-    }
+}
